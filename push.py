62509977c59a1290ab85ce856add49ff08641b24
root <root@localhost.localdomain>
2017-07-01 04:52:20 +0800

    (UPDATE) push.py

--- push.py
+++ push.py
@@ -3,5 +3,5 @@ import os
 
 commit = raw_input("Commit:")
 os.system('git add .')
-os.system('git commit -m "(%s)"' % commit)
+os.system('git commit -m "%s"' % commit)
 os.system('git push origin master')
